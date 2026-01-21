5868f66143d3d2828673996ca1708a0044dff5a1
yezdenz <103873073+yezdenz@users.noreply.github.com>
2026-01-21 13:26:52 +0800

    Update recursion1.py

--- recursion1.py
+++ recursion1.py
@@ -1,1 +1,31 @@
+def sumOfIntegers(n):
+    if n == 1:
+        print(1, end = "")
+        return 1
+    else:
+        print(n, end = " + ")
+        return n + sumOfIntegers(n-1)
+totalSum = sumOfIntegers(5)
+print(" =", totalSum)
+
+def factorialOfIntegers(n):
+    if n == 1:
+        print (1, end = "")
+        return 1
+    else:
+        print(n, end = " + ")
+        return n * factorialOfIntegers(n-1)
+totalFactorial = factorialOfIntegers(10)
+
+print(" =", totalFactorial)
+
+def fibonacciSequence(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacciSequence(n-1) + fibonacciSequence(n-2)
+    
+print(fibonacciSequence(56))
 
